eab5529fb0307c036209b48ba64c52c626e59062
DianaB177 <anaidb77@gmail.com>
2023-01-10 10:54:07 +0100

    jmm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Use State</h1>
+        <h1>Use State</h1>
+       <Counter />
       </header>
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Counter = () => {
+    const estadoInicial = 0;
+    let [contador, setContador] = useState(estadoInicial);
+    const handleSumar = () => {
+    setContador(contador +1);
+    }
+    const handleRestar = () => {
+    setContador(contador -1);
+    }
+    const handleReset = () => {
+    setContador(estadoInicial);
+    }
+
   return (
-    <div>Counter</div>
+    <div>
+        <h2>Counter</h2>
+        <hr />
+        <button onClick={handleSumar()}>Sumar</button>
+        <button onClick={handleRestar()}>Restar</button>
+        <button onClick={handleReset()}>Resetear</button>
+        <h4>{contador}</h4>
+    </div>
   )
 }
 
-export default Counter
+export default Counter;
